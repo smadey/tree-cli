83aa4f38d829aa20259fba6e0678a1c686b78c33
p_chrzhang(张成荣) <p_chrzhang@tencent.com>
2016-05-11 14:24:42 +0800

    重构

---
+++ README.md
@@ -0,0 +1,12 @@
+# tree-dir
+显示目录树
+
+##安装
+    $ npm install smadey@tree-dir -g
+
+##使用
+    $ tree-dir <dirname>
+
+排除相应目录
+
+    $ tree-dir <dirname> -i <ignoredir>
---
+++ index.js
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+
+var parseArgs = require('minimist');
+var tree = require('./lib/tree');
+var pkg = require('./package.json');
+
+var args = parseArgs(process.argv.slice(2), {
+  alias: {
+    h: 'help',
+    s: 'source',
+    i: 'ignore',
+    o: 'output',
+  }
+});
+
+if (args.help) {
+  console.log('Version: ' + pkg.version + '\n');
+  console.log('Usage: tree-dir -s <sourceDir> -i <ignoreDir1> -o <outputDir>\n');
+  console.log('Commands:');
+  console.log('tree-dir\tShow tree of current dir\n');
+  console.log('Options:');
+  console.log('-s <sourceDir>\tShow dir\n');
+  console.log('-i <ignoreDir>\tIgnore dir\n');
+  console.log('-o <outputDir>\tOutput dir\n');
+} else {
+  tree.run(args, function(err, result) {
+    if (err) {
+      console.error('ERROR: ' + err);
+      process.exit(1);
+    }
+  });
+}
